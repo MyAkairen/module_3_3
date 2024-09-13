0f970a5a21e14235f4fb9f7013d2a2edadeae0e6
MyAkairen <myakairen@yandex.ru>
2024-09-13 18:25:53 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,14 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a,b,c)
+
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = ["сосиска", 2, True]
+print_params(*values_list)
+
+values_dict = {"a":False, "b":3, "c":"шампунь"}
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
